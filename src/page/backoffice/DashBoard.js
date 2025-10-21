95140606d2c3d321e34567da69ce6bdd09426017
teerakarn-note <340562teerakarn@gmail.com>
2025-10-21 10:34:59 +0700

    add

--- src/page/backoffice/DashBoard.js
+++ src/page/backoffice/DashBoard.js
@@ -1,23 +1,29 @@
 import Backoffice from "../../components/ฺBackoffice";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, plugins,} from 'chart.js';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, plugins, } from 'chart.js';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 function DashBoard() {
+    let dataFromApi = [];
+    const [data, setData] = useState({});
+    useEffect(() => {
+        fetchData();
+    }, []);
+    const fetchData = async () => {
+        setData({
+            labels: ['January', 'February', 'March', 'April', 'May'],
+            datasets: [
+                {
+                    label: 'Mothly Sales',
+                    data: [10,20,30,40],
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }
+            ]
 
-    const data = {
-        labels: ['January', 'February', 'March', 'April', 'May'],
-        datasets: [
-            {
-                label: 'Mothly Sales',
-                data: [10, 20, 30, 40, 50],
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }
-        ]
-
+        });
     }
     const options = {
         responsive: true,
