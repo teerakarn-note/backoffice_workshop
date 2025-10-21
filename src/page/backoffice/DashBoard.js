38df435e424c66050de911ec0e07b21efbaf05b7
teerakarn-note <340562teerakarn@gmail.com>
2025-10-21 08:44:42 +0700

    add

--- src/page/backoffice/DashBoard.js
+++ src/page/backoffice/DashBoard.js
@@ -1,5 +1,44 @@
-function DashBoard(){
-    return <>
-    </>
+import Backoffice from "../../components/ฺBackoffice";
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, plugins,} from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+function DashBoard() {
+
+    const data = {
+        labels: ['January', 'February', 'March', 'April', 'May'],
+        datasets: [
+            {
+                label: 'Mothly Sales',
+                data: [10, 20, 30, 40, 50],
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }
+        ]
+
+    }
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top'
+            },
+            title: {
+                display: true,
+                text: 'Monthly Seles Data'
+            },
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    }
+
+    return <Backoffice>
+        <Bar data={data} options={options} />
+    </Backoffice>
 }
 export default DashBoard;
